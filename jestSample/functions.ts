2088e9c7293b557442601063ab9d97695efa7c71
sushidesu <hugemerge@gmail.com>
2021-02-02 18:07:10 +0900

    依存オブジェクトを注入できるように修正

--- jestSample/functions.ts
+++ jestSample/functions.ts
@@ -12,12 +12,12 @@ export const asyncSumOfArray = (numbers: number[]): Promise<number> => {
 };
 
 export const asyncSumOfArraySometimesZero = (
-  numbers: number[]
+  numbers: number[],
+  save: DatabaseMock["save"]
 ): Promise<number> => {
   return new Promise((resolve): void => {
     try {
-      const database = new DatabaseMock(); // fixme: この関数をテストするには、DatabaseMockの使い方を変える必要がありそう！ヒント：依存性の注入
-      database.save(numbers);
+      save(numbers);
       resolve(sumOfArray(numbers));
     } catch (error) {
       resolve(0);
