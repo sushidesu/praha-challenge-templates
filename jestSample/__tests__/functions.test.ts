2fb410cbe5eeaee0725c3f0c5746da4a450043cd
sushidesu <hugemerge@gmail.com>
2021-02-02 12:58:24 +0900

    sumOfArray関数のテストを作成

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,1 +1,15 @@
-// todo: ここに単体テストを書いてみましょう！
+import { sumOfArray } from "../functions"
+
+describe("sumOfArray() は数の配列の要素を合計する", () => {
+  it("[1, 1] を渡すと 2 を返す", () => {
+    expect(sumOfArray([1, 1])).toBe(2)
+  })
+
+  it("[] を渡すとエラーになる", () => {
+    expect(() => sumOfArray([])).toThrowError()
+  })
+
+  it("文字列を渡すとエラーになる", () => {
+    expect(() => sumOfArray("[1, 1]")).toThrowError()
+  })
+})
