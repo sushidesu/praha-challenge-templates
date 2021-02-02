6bd75ed9b5ea5ceb184a6063ba7e746e176aa96e
sushidesu <hugemerge@gmail.com>
2021-02-02 19:41:51 +0900

    依存オブジェクトを注入できるように修正

--- jestSample/functions.ts
+++ jestSample/functions.ts
@@ -26,10 +26,10 @@ export const asyncSumOfArraySometimesZero = (
 };
 
 export const getFirstNameThrowIfLong = async (
-  maxNameLength: number
+  maxNameLength: number,
+  getFirstName: NameApiService["getFirstName"]
 ): Promise<string> => {
-  const nameApiSerivce = new NameApiService(); // fixme: この関数をテストするには、NameApiServiceの使い方を変える必要がありそう！ヒント：依存性の注入
-  const firstName = await nameApiSerivce.getFirstName();
+  const firstName = await getFirstName();
 
   if (firstName.length > maxNameLength) {
     throw new Error("first_name too long");
