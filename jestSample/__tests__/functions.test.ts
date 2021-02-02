218714fb873c4641708b5297c66f3b0a22b91c95
sushidesu <hugemerge@gmail.com>
2021-02-02 18:11:27 +0900

    asyncSumOfArraySometimesZero関数のテストを作成

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,4 +1,9 @@
-import { sumOfArray, asyncSumOfArray } from "../functions"
+import {
+  sumOfArray,
+  asyncSumOfArray,
+  asyncSumOfArraySometimesZero,
+} from "../functions"
+import { DatabaseMock } from "../util"
 
 describe("sumOfArray() は数の配列の要素を合計する", () => {
   it("[1, 1] を渡すと 2 を返す", () => {
@@ -25,5 +30,41 @@ describe("asyncSumOfArray() は数の配列の要素を非同期で合計する"
 
   it("文字列を渡すとエラーになる", async () => {
     await expect(asyncSumOfArray("[1, 1]")).rejects.toThrowError()
+  })
+})
+
+describe("asyncSumOfArraySometimesZero() は数の配列をデータベースに保存し、数の配列の要素を合計する", () => {
+  let mockSaveAlwaysSuccess: DatabaseMock["save"]
+  beforeEach(() => {
+    mockSaveAlwaysSuccess = jest.fn()
+  })
+
+  describe("エラーが発生しない場合 数の配列の要素を合計する", () => {
+    it("[1, 1] を渡すと 2 を返す", async () => {
+      await expect(
+        asyncSumOfArraySometimesZero([1, 1], mockSaveAlwaysSuccess)
+      ).resolves.toBe(2)
+    })
+  })
+
+  describe("エラーが発生した場合 0 を返す", () => {
+    it("[] を渡すと 0 を返す", async () => {
+      await expect(asyncSumOfArraySometimesZero([], mockSaveAlwaysSuccess)).resolves.toBe(0)
+    })
+
+    it("文字列を渡すと 0 を返す", async () => {
+      await expect(
+        asyncSumOfArraySometimesZero("[1, 1]", mockSaveAlwaysSuccess)
+      ).resolves.toBe(0)
+    })
+
+    it("DatabaseMock.save() が失敗した場合は 0 を返す", async () => {
+      const mockSaveAlwaysThrowError = jest.fn(() => {
+        throw Error()
+      })
+      await expect(
+        asyncSumOfArraySometimesZero([1, 1], mockSaveAlwaysThrowError)
+      ).resolves.toBe(0)
+    })
   })
 })
