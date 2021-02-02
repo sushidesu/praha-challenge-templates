fc35ac4004970a1e904ee354aabbae41d9487e80
sushidesu <hugemerge@gmail.com>
2021-02-02 14:24:13 +0900

    asyncSumOfArray関数のテストを作成

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,4 +1,4 @@
-import { sumOfArray } from "../functions"
+import { sumOfArray, asyncSumOfArray } from "../functions"
 
 describe("sumOfArray() は数の配列の要素を合計する", () => {
   it("[1, 1] を渡すと 2 を返す", () => {
@@ -11,5 +11,19 @@ describe("sumOfArray() は数の配列の要素を合計する", () => {
 
   it("文字列を渡すとエラーになる", () => {
     expect(() => sumOfArray("[1, 1]")).toThrowError()
+  })
+})
+
+describe("asyncSumOfArray() は数の配列の要素を非同期で合計する", () => {
+  it("[1, 1] を渡すと 2 を返す", async () => {
+    await expect(asyncSumOfArray([1, 1])).resolves.toBe(2)
+  })
+
+  it("[] を渡すとエラーになる", async () => {
+    await expect(asyncSumOfArray([])).rejects.toThrowError()
+  })
+
+  it("文字列を渡すとエラーになる", async () => {
+    await expect(asyncSumOfArray("[1, 1]")).rejects.toThrowError()
   })
 })
