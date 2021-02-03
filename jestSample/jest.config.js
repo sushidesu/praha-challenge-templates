f80da56abb46ff166510bd539ea90a53a6cc1b31
sushidesu <hugemerge@gmail.com>
2021-02-03 15:48:52 +0900

    全てのテスト結果を表示するように設定を変更

--- jestSample/jest.config.js
+++ jestSample/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   collectCoverage: true,
-  collectCoverageFrom: ["**/functions.ts"],
+  verbose: true,
   globals: {
     "ts-jest": {
       diagnostics: {
